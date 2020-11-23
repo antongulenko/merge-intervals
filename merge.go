b1ca1c14f880eea5ca639d2023ad668405373370
Anton Gulenko <anton.gulenko@googlemail.com>
2020-11-23 13:00:23 +0100

    Added tests and finished implementation of MergeIntervals() function

--- merge.go
+++ merge.go
@@ -1,23 +1,91 @@
 package merge_intervals
 
-import "fmt"
+import (
+	"sort"
+)
 
+// Interval contains the lower and upper bounds of an integer interval
 type Interval struct {
 	From int
 	To   int
 }
 
-func MergeIntervals(intervals ...Interval) interface{} {
-	return nil
+// Merge attempts to merge the receiving interval with the argument. The merge succeeds when the intervals overlap,
+// in this case the merged interval is returned, along with a true-valued flag indicating the success. Otherwise,
+// a false-valued flag indicates that the intervals do not overlap. This operation is symmetrical (the receiver and the
+// argument can be exchanged with the same result)..
+//
+// In case of a successful merge, the result is always a correct interval, i.e. result.From <= result.To
+func (i Interval) Merge(other Interval) (result Interval, overlap bool) {
+	other = other.Fix()
+	overlap = i.Contains(other.From) || i.Contains(other.To)
+	if overlap {
+		result = i.Fix()
+		if result.From > other.From {
+			result.From = other.From
+		}
+		if result.To < other.To {
+			result.To = other.To
+		}
+	}
+	return
 }
 
-func Intervals(bounds ...int) []Interval {
-	if len(bounds)%2 != 0 {
-		panic(fmt.Sprintf("Cannot construct intervals from uneven number of bounds"))
+// Contains returns whether the receiving interval contains the argument integer (both the lower and upper bounds
+// work inclusively).
+func (i Interval) Contains(num int) bool {
+	return num >= i.From && num <= i.To
+}
+
+// Fix swaps the From and To fields of the receiving interval, if To < From
+func (i Interval) Fix() Interval {
+	if i.To < i.From {
+		i.From, i.To = i.To, i.From
 	}
-	result := make([]Interval, len(bounds)/2)
-	for i := range result {
-		result[i] = Interval{bounds[i*2], bounds[i*2+1]}
+	return i
+}
+
+// Intervals contain a slice of Interval data. Intervals implements sort.Interface to sort all intervals based on their
+// lower bound.
+type Intervals []Interval
+
+func (i Intervals) Len() int {
+	return len(i)
+}
+
+func (i Intervals) Swap(x, y int) {
+	i[x], i[y] = i[y], i[x]
+}
+
+func (i Intervals) Less(x, y int) bool {
+	return i[x].From < i[y].From
+}
+
+func MergeIntervals(intervals Intervals) Intervals {
+	if len(intervals) == 0 {
+		return intervals
 	}
+	sort.Sort(intervals)
+	var result Intervals
+
+	// Initialize the aggregation variable to the lowest interval
+	current := intervals[0]
+	intervals = intervals[1:]
+
+	// Iterate the sorted intervals and keep merging them, until encountering a non-overlapping interval.
+	// Since the intervals are sorted, a non-overlapping interval indicates the beginning of a new output interval.
+	for _, interval := range intervals {
+		merged, overlap := current.Merge(interval)
+		if overlap {
+			// Intervals overlap: continue merging.
+			current = merged
+		} else {
+			result = append(result, current.Fix())
+			current = interval
+		}
+	}
+
+	// At the end, the current aggregation variable is the last result interval.
+	result = append(result, current.Fix())
 	return result
 }
--- merge_test.go
+++ merge_test.go
@@ -25,13 +25,55 @@ func TestMergeIntervals(t *testing.T) {
 	suite.Run(t, new(MergeIntervalsTestSuite))
 }
 
-func (s *MergeIntervalsTestSuite) TestIntervalsConstruction() {
-	s.Equal([]Interval{}, Intervals())
-	s.Equal([]Interval{{1, 2}}, Intervals(1, 2))
-	s.Equal([]Interval{{1, 2}, {3, 4}, {5, 6}}, Intervals(1, 2, 3, 4, 5, 6))
+func (s *MergeIntervalsTestSuite) TestContains() {
+	s.True(Interval{5, 10}.Contains(7))
+	s.True(Interval{5, 10}.Contains(5))
+	s.True(Interval{5, 10}.Contains(10))
+	s.False(Interval{5, 10}.Contains(2))
+	s.False(Interval{5, 10}.Contains(12))
+}
+
+func (s *MergeIntervalsTestSuite) TestMergeTwoIntervals() {
+	// Test non-overlapping intervals
+	result, overlap := Interval{5, 10}.Merge(Interval{15, 20})
+	s.False(overlap)
+	s.Equal(Interval{}, result)
+
+	// Test argument overlapping upper part of receiver
+	result, overlap = Interval{5, 10}.Merge(Interval{7, 15})
+	s.True(overlap)
+	s.Equal(Interval{5, 15}, result)
+
+	// Test argument overlapping lower part of receiver
+	result, overlap = Interval{5, 10}.Merge(Interval{2, 8})
+	s.True(overlap)
+	s.Equal(Interval{2, 10}, result)
+
+	// Test argument equals receiver
+	result, overlap = Interval{5, 10}.Merge(Interval{5, 10})
+	s.True(overlap)
+	s.Equal(Interval{5, 10}, result)
+
+	// Test argument directly follows receiver
+	result, overlap = Interval{5, 10}.Merge(Interval{10, 15})
+	s.True(overlap)
+	s.Equal(Interval{5, 15}, result)
 }
 
 func (s *MergeIntervalsTestSuite) TestExample() {
-	merged := MergeIntervals(Interval{25, 30}, Interval{2, 19}, Interval{14, 23}, Interval{4, 8})
-	s.Equal(Intervals(2, 23, 25, 30), merged)
+	merged := MergeIntervals(Intervals{{25, 30}, Interval{2, 19}, Interval{14, 23}, Interval{4, 8}})
+	s.Equal(Intervals{{2, 23}, {25, 30}}, merged)
+}
+
+func (s *MergeIntervalsTestSuite) TestMergeEmptyIntervals() {
+	s.Equal(Intervals{}, MergeIntervals(Intervals{}))
+}
+
+func (s *MergeIntervalsTestSuite) TestMergeEqualIntervals() {
+	s.Equal(Intervals{{5, 10}}, MergeIntervals(Intervals{{5, 10}, {5, 10}, {5, 10}}))
+}
+
+func (s *MergeIntervalsTestSuite) TestMergeIncorrectIntervals() {
+	// Test with intervals where From > To
+	s.Equal(Intervals{{5, 15}, {20, 40}}, MergeIntervals(Intervals{{15, 8}, {5, 10}, {40, 20}}))
 }
